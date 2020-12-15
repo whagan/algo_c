7b24667ab49a3cf2abdeab0963c90cacb77013b7
Will Hagan <william_hagan@hotmail.com>
2020-12-14 22:52:29 -0600

    queues makefile

--- queues/include/queue.h
+++ queues/include/queue.h
@@ -17,7 +17,6 @@
 *                                                                            *
 *****************************************************************************/
 
-typedef List Queue;
 
 /*****************************************************************************
 *                                                                            *
@@ -25,16 +24,10 @@ typedef List Queue;
 *                                                                            *
 *****************************************************************************/
 
-#define queue_init list_init
 
-#define queue_destroy list_destroy
 
-int queue_enqueue(Queue *queue, const void *data);
 
-int queue_dequeue(Queue *queue, void **data);
 
-#define queue_peek(queue) ((queue)->head == NULL ? NULL : (queue)->head->data)
 
-#define queue_size list_size
 
 #endif
